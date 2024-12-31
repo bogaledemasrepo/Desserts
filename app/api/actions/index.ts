00fa0de7da2398caca66e3b689cd9e0cfb868cd2
Admin <kali@kali>
2024-12-31 12:02:03 +0300

    start restracturing

--- app/api/actions/index.ts
+++
@@ -1,7 +0,0 @@
-export const useData = {
-  session: {
-    userId: "user1",
-    name: "Bogale Demas",
-    photo: "/profile/image/user1.png",
-  },
-};
--- db/index.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-const dburl = process.env.DB_URL!;
-mongoose.connect(dburl);
-mongoose.connection.on("connect", () => {
-  console.log("Connected successfully!!!");
-});
-mongoose.connection.on("error", (err) => {
-  console.log("Error to connect to database!", err);
-});
