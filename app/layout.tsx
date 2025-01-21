392ee602582fcefe8c33fe1be21297e3134b31a1
Admin <kali@kali>
2025-01-21 11:52:00 +0300

    favicon fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <title>Hallo from title</title>
+        </Head>
         {children}
       </body>
     </html>
