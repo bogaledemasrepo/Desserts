417e4f793fa2cdd944c5e9ad478676bfba7d5f4f
Bogale Demas <bogidemas@gmail.com>
2025-02-10 13:23:51 +0300

    little error fix on sign-in

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,5 @@
 import { SignIn } from "@clerk/nextjs";
-
+export const runtime = "edge";
 export default function Page() {
   return (
     <div className="w-full min-h-screen flex items-center justify-center">
